a9e3e3f866fac8859fd4ff1def9d777695616dc8
00buku <clydescottwilliamsjr@gmail.com>
2022-12-13 11:01:39 -0600

    configured deployment code

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-// load the env consts
+
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-// session middleware
+
 const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
+const MongoStore = require('connect-mongo');
+
 const indexRouter = require('./routes/index');
 const storesRouter = require('./routes/stores');
 const itemsRouter = require('./routes/items');
-// create the Express app
+
 const app = express();
 
-// connect to the MongoDB with mongoose
+
 require('./config/database');
-// configure Passport
+
 require('./config/passport');
 
 
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -31,8 +33,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// mount the session middleware
+
+
 app.use(session({
+  store: MongoStore.create({
+    mongoUrl: process.env.DATABASE_URL
+  }),
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
@@ -42,20 +48,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// Add this middleware BELOW passport middleware
+
 app.use(function (req, res, next) {
-  res.locals.user = req.user; // assinging a property to res.locals, makes that said property (user) availiable in every
-  // single ejs view
+  res.locals.user = req.user;
+
   next();
 });
 
-// mount all routes with appropriate base paths
+
 app.use('/', indexRouter);
 app.use('/stores', storesRouter);
 app.use('/', itemsRouter);
 
 
-// invalid request, send 404 page
+
 app.use(function(req, res) {
   res.status(404).send('Cant find that!');
 });
